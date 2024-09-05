a54d0df9b5388da07be02158f5580b580814ea65
Matt Dean <mattjdean88@gmail.com>
2024-09-05 02:46:41 +0100

    0245 05/09/24

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ const typedText = [document.querySelector('#typedText'), document.querySelector(
 const text = "I'm a web developer."; 
 
 
-function typedTextEffect(version) {
-    let index = 0; // Character index
-    function type() {
-        if (index < text.length) { //Loops through each character in the text
-            version.textContent += text.charAt(index);
-            index++;
-            setTimeout(type, 150); // Typing speed (in ms)
-        }
-    }
-    type();
-}
+// function typedTextEffect(version) {
+//     let index = 0; // Character index
+//     function type() {
+//         if (index < text.length) { //Loops through each character in the text
+//             version.textContent += text.charAt(index);
+//             index++;
+//             setTimeout(type, 150); // Typing speed (in ms)
+//         }
+//     }
+//     type();
+// }
   
 
-// Triggers the typing effect
-document.addEventListener('DOMContentLoaded', () => { 
-    typedText.forEach((version) => {
-    if (window.getComputedStyle(version).display !== 'none') { //Applies effect only to visible instance of h2 text.
-        typedTextEffect(version);
-    }
-    });
-});
+// // Triggers the typing effect
+// document.addEventListener('DOMContentLoaded', () => { 
+//     typedText.forEach((version) => {
+//     if (window.getComputedStyle(version).display !== 'none') { //Applies effect only to visible instance of h2 text.
+//         typedTextEffect(version);
+//     }
+//     });
+// });
 
 
 
